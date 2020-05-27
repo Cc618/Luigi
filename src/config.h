ecd87b1e5b0539abba9d0ff2e160f2b8f21b9cf2
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-05-27 18:45:58 +0200

    Removed config.h

--- src/config.h
+++
@@ -1,7 +0,0 @@
-// Luigi version
-
-#include <string>
-
-#define LUIGI_VERSION_MAJOR 0
-#define LUIGI_VERSION_MINOR 1
-#define LUIGI_VERSION_STR (std::to_string(LUIGI_VERSION_MAJOR) + "." + std::to_string(LUIGI_VERSION_MINOR))
