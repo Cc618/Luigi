220a616b1b3b29689d87b08c251149e0829e16f1
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-05-30 17:39:18 +0200

    io: Init, added read_txt_file

---
+++ src/io.cpp
@@ -0,0 +1,20 @@
+#include "io.h"
+#include <fstream>
+#include "error.h"
+
+using namespace std;
+
+std::string read_txt_file(const std::string& path)
+{
+    ifstream f(path);
+
+    Error::check(f.is_open(), "Can't open file '" + path + "'");
+
+    // TODO : Use algorithm
+    string s;
+    string line;
+    while (getline(f, line))
+        s += line;
+    
+    return s;
+}
